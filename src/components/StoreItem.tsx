e48e86c667147ca7ccd5b6572ca249d086daf253
AndreiLhu <leahu.andreilucian@gmail.com>
2023-05-25 16:46:07 +0200

    Small changes. Adding formatCurrency to price

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,4 +1,5 @@
 import { Card } from 'react-bootstrap';
+import { formatCurrency } from '../utilities/formatCurrency';
 
 type StoreItemProps = {
   id: number;
@@ -18,10 +19,10 @@ export function StoreItem({ id, name, gender, price, imgUrl }: StoreItemProps) {
         style={{ objectFit: 'cover' }}
       />
       <Card.Body className="d-flex flex-column">
-        <Card.Title className="d-flex justify-content-space-between align-items-baseline mb-4">
+        <Card.Title className="d-flex justify-content-between align-items-baseline mb-4">
           <span className="fs-2">{name}</span>
-          <span className="ms-2">{gender}</span>
-          <span className="ms-2">{price}</span>
+          <span className="ms-2 text-muted">{gender}</span>
+          <span className="ms-2 text-muted">{formatCurrency(price)}</span>
         </Card.Title>
       </Card.Body>
     </Card>
